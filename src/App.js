a0eda1d15511c39d9b96d789bd805ebd57183de7
GlebTc <bob88tikhonov@gmail.com>
2022-05-25 22:47:02 -0400

    Set up the structure for components and SASS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+
+import About from './components/pages/About'
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import './sass/main.scss';
 
 function App() {
   return (
-    <h1>Starting Page</h1>
+    <Router>
+      <NavBar />
+        <Routes>
+          <Route exact path="/" element={<About />} />       
+        </Routes>
+      <Footer />
+    </Router>
   );
 }
 
