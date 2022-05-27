c2876b808e0c6b7b87278a021d01d421388350ea
GlebTc <bob88tikhonov@gmail.com>
2022-05-27 13:11:25 -0400

    Created files for image data and the slider

---
+++ src/components/utilities/Images_Data.js
@@ -0,0 +1,36 @@
+let imagesArray = [
+    {
+        img__src: "https://images.unsplash.com/photo-1521783593447-5702b9bfd267?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=904&q=80",
+        alt_msg: "Bathroom 1"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1507652313519-d4e9174996dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        alt__msg: "Bathroom 3"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1600585152220-90363fe7e115?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        alt__msg: "Kitchen 3"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1613688684285-472f9bba333d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+        alt__msg: "Bathroom 4"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1502005097973-6a7082348e28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        img__msg: "Kitchen 4"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1646592474103-cfd22d1d9e34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        img__msg: "Basement 1"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1646592474273-86049d4f3575?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        img__msg: "Basement 2"
+    },
+    {
+        img__src: "https://images.unsplash.com/photo-1630699376289-b62375a35505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        img__msg: "Living Room 1"
+    }
+]
+
+export default imagesArray
