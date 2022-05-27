7e4453a4955fcfb822535b5ea4daa3f7e0040b32
GlebTc <bob88tikhonov@gmail.com>
2022-05-27 17:23:22 -0400

    Added some content and minor styling adjustments

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="main__container">
-            <h1>Home Page</h1>
+            <h1>Our Mission Statement</h1>
+            <p>
+                To perform for our customers at the highest level of quality service at fair and market competitive prices.  To ensure the longevity of our company through repeat and referral business achieved by customer satisfaction in all areas including timeliness, attention to detail and service-minded attitudes.  To maintain the highest levels of professionalism, integrity, honesty and fairness in our relationships with our suppliers, subcontractors, professional associates and customers. 
+            </p>
         </div>        
     )
 }
