26ec8472d29dcbd3f27f42a82e83efc67a18a402
GlebTc <bob88tikhonov@gmail.com>
2022-05-26 23:25:33 -0400

    Did not do anything gallery related yest, mostly played with UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <div className="footer__container">
-            
+            <h1>Copyright &copy; Gleb Tc 2021</h1>
+            <div className="footnote__connect">
+                <a className="instagram__link" href="https://www.instagram.com/edo_renoz/?hl=en">
+                    <FontAwesomeIcon className="social__media__icons" icon={faInstagram} />
+                </a>                
+            </div>
         </div>
+
     )
 }
 
